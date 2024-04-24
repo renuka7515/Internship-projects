160b100dd075a3f9637360c6d2e26ae4e52807d3
renuka7515 <133946832+renuka7515@users.noreply.github.com>
2024-04-24 23:05:49 +0530

    Update app.py

--- GEN_AI_APPS/Langchain_Rag _Application/app.py
+++ GEN_AI_APPS/Langchain_Rag _Application/app.py
@@ -33,7 +33,7 @@ chat_template = ChatPromptTemplate.from_messages([
 ])
 
 # Initialize chat model
-chat_model = ChatGoogleGenerativeAI(google_api_key="AIzaSyC2Bztff9XtDCDrCJfMJ8py9JaT8VkwSlY", 
+chat_model = ChatGoogleGenerativeAI(google_api_key=key, 
                                     model="gemini-1.5-pro-latest")
 
 # Initialize output parser
